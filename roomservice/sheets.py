c5fcb891f518d29efe185c9f1bd0968da8149d65
Rory <rmurdock@woolworths.com.au>
2019-08-27 06:56:37 +0000

    initial commit

---
+++ roomservice/sheets.py
@@ -0,0 +1,51 @@
+import httplib2
+import sys
+import os
+
+from apiclient import discovery
+from google.oauth2 import service_account
+
+class sheets:
+    def __init__(self, spreadsheet_id, secret_file='config/client_secret.json'):
+        scopes = ["https://www.googleapis.com/auth/spreadsheets.readonly", 'https://www.googleapis.com/auth/drive']
+
+        if not os.path.isfile(secret_file):
+            print("No API credentials found")
+            sys.exit()
+
+        credentials = service_account.Credentials.from_service_account_file(secret_file, scopes=scopes)
+        self.service = discovery.build('sheets', 'v4', credentials=credentials)
+        self.spreadsheet_id = spreadsheet_id
+
+    def get(self, range):
+        result = self.service.spreadsheets().values().get(spreadsheetId=self.spreadsheet_id, range=range).execute()
+
+        values = result.get('values', [])
+
+        return values
+    
+    def write(self, range, data):
+        result = self.service.spreadsheets().values().update(spreadsheetId=self.spreadsheet_id, range=range, body=data, valueInputOption='USER_ENTERED').execute()
+
+        return result
+    
+    def clear_sheet(self, sheet, range):
+        request = self.service.spreadsheets().values().clear(spreadsheetId=self.spreadsheet_id,range=range,body={}).execute()
+
+        return request
+
+    def delete_row(self, start, end, sheet):
+        request = {}
+        request['deleteDimension'] = {}
+        request['deleteDimension']['range'] = {}
+        request['deleteDimension']['range']['sheetId'] = sheet
+        request['deleteDimension']['range']['startIndex'] = start
+        request['deleteDimension']['range']['endIndex'] = end
+        request['deleteDimension']['range']['dimension'] = "ROWS"
+        body = {}
+        body['requests'] = []
+        body['requests'].append(request)
+
+        request = self.service.spreadsheets().batchUpdate(spreadsheetId=self.spreadsheet_id, body=body).execute()
+
+        return request
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+# name = roomservice
+
+setuptools.setup(
+    name="roomservice",
+    version="0.0.6",
+    author="Rory Murdock",
+    author_email="rmurdock@woolworths.com.au",
+    description="A Library for interacting with Google Sheets, changing your sheets",
+    # long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://github.com/rorymurdock/WSO-UEM-Py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        # "Development Status :: 5 - Production/Stable",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'google-auth-httplib2',
+          'google-api-python-client',\
+    ]
+)
